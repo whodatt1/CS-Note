e71d3aaa09077599d5ee3e7519de4d34cc5c05ab
whodatt1 <codingman875@naver.com>
2024-07-07 12:05:49 +0900

    update:2024-07-07 12:05

---
+++ Docker/docker_study/ex09/my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <h1>상품목록페이지</h1>
+      <hr/>
+      <div>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
