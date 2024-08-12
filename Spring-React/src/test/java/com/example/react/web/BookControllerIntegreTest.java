4b298049402f01c1cb68a94cbef16d904d1ae50f
whodatt1 <codingman875@naver.com>
2024-08-12 21:26:50 +0900

    update:2024-08-12 21:26

--- Spring-React/src/test/java/com/example/react/web/BookControllerIntegreTest.java
+++ Spring-React/src/test/java/com/example/react/web/BookControllerIntegreTest.java
@@ -1,11 +1,25 @@
 package com.example.react.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.react.domain.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 통합 테스트 (컨트롤러로 전체 스프링을 테스트한다.)
@@ -17,6 +31,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @Transactional = 각각의 테스트 함수가 종료될 때마다 트랜잭션을 rollback 시켜주는 어노테이션
  */
 
+@Slf4j
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK) // 실제 톰캣을 올리는게 아니라, 다른 톰캣으로 테스트 
@@ -25,4 +40,24 @@ public class BookControllerIntegreTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	// BDDMockito 패턴 given, when, then
+	@Test
+	public void save_테스트() throws Exception {
+		// given (테스트를 하기 위한 준비)
+		Book book = new Book(null, "스프링 따라하기", "코스");
+		String content = new ObjectMapper().writeValueAsString(book); // Object를 JSON으로 바꾸는 함수
+		
+		// when (테스트 실행)
+		ResultActions resultActions = mockMvc.perform(post("/book")
+			   .contentType(MediaType.APPLICATION_JSON_VALUE) // 요청
+			   .content(content) // 내용
+			   .accept(MediaType.APPLICATION_JSON_VALUE)); // 응답
+		
+		// then (검증)
+		resultActions
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.title").value("스프링 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+			
+	}
 }
